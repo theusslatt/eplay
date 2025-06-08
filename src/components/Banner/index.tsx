c07225e3a0816a09311a62d9375ab523e3b4fd42
Matheus Silveira <theunewba@gmail.com>
2025-06-08 02:51:35 -0300

    criando o componente de tag

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,9 +1,11 @@
 import { Imagem, Precos, Titulo } from './styles'
 import bannerImg from '../../assets/images/banner-homem-aranha.png'
+import Tag from '../Tag'
 
 const Banner = () => (
   <Imagem style={{ backgroundImage: `url(${bannerImg})` }}>
     <div className="container">
+      <Tag size="big">Destaque do dia</Tag>
       <Titulo>Marvel&apos;s Spider-Man: Miles Morales PS4 & PS5</Titulo>
       <Precos>
         De <span>R$ 250,00</span> <br />
